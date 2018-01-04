599bfd944046bdea73e6c1aa125afbe31d503822
howyi <qrbys@outlook.com>
2018-01-04 19:19:53 +0900

    add Twitter send function

--- src/ServiceMonitor/Twitter/TwitterEvent.php
+++ src/ServiceMonitor/Twitter/TwitterEvent.php
@@ -2,8 +2,17 @@
 
 namespace ServiceMonitor\Twitter;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
 use ServiceMonitor\EventInterface;
 
 abstract class TwitterEvent implements EventInterface
 {
+    protected $connection;
+    protected $self;
+
+    public function set(TwitterOAuth $connection, \stdClass $self): void
+    {
+        $this->connection = $connection;
+        $this->self = $self;
+    }
 }
--- src/ServiceMonitor/Twitter/UserStreamMonitor.php
+++ src/ServiceMonitor/Twitter/UserStreamMonitor.php
@@ -2,12 +2,26 @@
 
 namespace ServiceMonitor\Twitter;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
 use Spatie\TwitterStreamingApi\UserStream;
 
 class UserStreamMonitor extends StreamMonitor
 {
     public function start(): void
     {
+        $connection = new TwitterOAuth(
+            $this->consumerKey(),
+            $this->consumerSecret(),
+            $this->accessToken(),
+            $this->accessTokenSecret()
+        );
+
+        $self = $connection->get('account/verify_credentials');
+
+        foreach ($this->events as $event) {
+            $event->set($connection, $self);
+        }
+
         $stream = new UserStream(
             $this->accessToken(),
             $this->accessTokenSecret(),
